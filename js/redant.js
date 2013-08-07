f68307c77c49f8a02d7422fccd03e5db9791676c
song <yusongone@gmail.com>
2013-08-07 08:46:15 +0800

    add setFrame of time

--- js/redant.js
+++ js/redant.js
@@ -6,44 +6,71 @@ var game=window.game||(function(){
 		 * */
 
 		//create canvas and context;
-		function _createTempCanvas=function(){
+		function _createTempCanvas(){
 			var tempCanvas=document.createElement("canvas");
 				tempCanvas.width=this.width;
 				tempCanvas.height=this.height;
-			this._tempCanvas.tempCanvas;
-			this._ctx=tempCanvas.getContext("2d");
+			this.spriteCanvas=tempCanvas;
+			this.spriteCtx=tempCanvas.getContext("2d");
 		};
 
 		/*
 		 *sprite entity;
 		 * */
 		var _sprite=function(width,height){
-			this.width=width;
-			this.height=height;
+			this.name=_game.funLib.getRandom();
+			this.width=width||20;
+			this.height=height||20;
 			this.ImageData={};
 			this.childSprite={};
+			this.FrameFun={};
 			_createTempCanvas.call(this);
+			game.addSprite(this);
+			this.setFrame("drawNyCanvas",this.drawMyCanvas,10);
 		};	
 		_sprite.prototype.test=function(){
 		
 		}
+		_sprite.prototype.drawMyCanvas=function(animateData){
+			var ctx=animateData.ctx;
+				ctx.drawImage(this.spriteCanvas,0,0,this.width,this.height);
+		}
+		_sprite.prototype.oneFrameFun=function(animateData){
+				var time=animateData.time;
+				for(var i in this.FrameFun){
+					var temp=this.FrameFun[i];
+					if(temp.time<time-temp.index){
+						temp.index=time;
+						temp.call(this,animateData);
+					}
+				}
+				for(var j in this.childSprite){
+					var temp=this.childSprite[j];
+						temp.oneFrameFun();
+				}
+		};
 		_sprite.prototype.setImageData=function(name,json){
 				var tempAry=[];
-				var tempJson=json[i];
-				for(var j=0,i=tempJson.length;j<l;j++){
-						var CanvasData=tempJson[j];
+				var tempJson=json;
+				for(var j=0,l=tempJson.data.length;j<l;j++){
+						var CanvasData=tempJson.data[j];
 					var canvas=document.createElement("canvas");
-						canvas.width=oneCanvaData.width;
-						canvas.height=oneCanvaData.height;
+						canvas.width=CanvasData.width;
+						canvas.height=CanvasData.height;
 					var ctx=canvas.getContext("2d");
 						ctx.drawImage(json.img,CanvasData.x,CanvasData.y,CanvasData.width,CanvasData.height);
 						tempAry.push(canvas);
 				}
 				this.ImageData[name]=tempAry;
 		};
-		_sprite.prototype.updateFrame=function(){
-
-		}
+		_sprite.prototype.setFrame=function(name,fun,time){
+				fun.time=time;
+				fun.index=0;
+				this.FrameFun[name]=fun;
+		};
+		_sprite.prototype.removeFrame=function(name){
+				delete this.FrameFun[name];
+		};
 		return _sprite;
 	})();
 
@@ -52,8 +79,11 @@ var game=window.game||(function(){
 	 *Animation entity;
 	 * */
 	var Animation=(function(){
+		var _canvas=document.createElement("canvas");
+			_canvas.width=100;
+			_canvas.width=200;
+			_ctx=_canvas.getContext("2d");
 		var _RAF=window.mozRequestAnimationFrame||window.requestAnimationFrame||window.webkitRequestAnimationFrame;
-		var _canvas,_ctx;
 		var _childList;
 		//
 		var _startAnimateList={};
@@ -79,29 +109,30 @@ var game=window.game||(function(){
 		function _paintOverSprite() {
 
 		};
-		function _updateSprite(){
+		function _updateSprite(time){
 			for(var i in _childList){
-
+				_childList[i].oneFrameFun(time);
 			}
 		};
-		function _oneFrame(){
-			_tick();
-			_clearScreen();
-			_startAnimate(time);
-			_paintUnderSprite();	
-			_updateSprite();	
-			_paintOverSprite();
-			_endAnimate();
-			_RAF(oneFrame);
+		var _AnimateData={};
+			_AnimateData.ctx=_ctx;
+		function _oneFrame(time){
+			//_tick();
+			_AnimateData.time=time;
+			_clearScreen(_AnimateData);
+			_startAnimate(_AnimateData);
+			_paintUnderSprite(_AnimateData);	
+			_updateSprite(_AnimateData);	
+			_paintOverSprite(_AnimateData);
+			_endAnimate(_AnimateData);
+			_RAF(_oneFrame);
 		};
 		return {
-			start:function(canvas){
-				_canvas=canvas;
-				_ctx=canvas.getContext("2d");
+			start:function(){
 				_oneFrame();
 			},	
-			setChildList:function(childList){
-				_childList=childList;
+			getCanvas:function(){
+				return _canvas;
 			},
 			pushStartAnimate:function(key,fun){
 				_startAnimateList[key]=fun;
@@ -114,7 +145,11 @@ var game=window.game||(function(){
 			},
 			removeEndAnimate:function(){
 				delete _endAnimateList[key];
+			},
+			setChildList:function(obj){
+				_childList=obj;
 			}
+
 		}	
 	})();
 
@@ -128,9 +163,24 @@ var game=window.game||(function(){
 			temp.constructor=sub;
 			sub.prototype=new temp();
 		};
+		var _randomJson={};
 		return {
-			"extend":_extend
-		
+			extend:_extend
+			,getRandom:function(b,bool){
+				var bb=b||10;
+					var bs=Math.pow(10,bb);
+				var sj=parseInt(bs*Math.random(1));
+				if(_randomJson[sj]&&bool){
+					var ssj=_getRandom(b);	
+					_randomJson[ssj]=ssj;
+					return ssj;
+				}else if(_randomJson[sj]==0){
+					console.error("can't get random!");
+				}else{
+					_randomJson[sj]=sj;
+					return sj;
+				}
+			}
 		};
 	})();
 
@@ -138,12 +188,10 @@ var game=window.game||(function(){
 	 * main of game engine 
 	 */
 	var _game=(function(){
-		var canvas=document.createElement("canvas");
-			canvas.width=100;
-			canvas.width=200;
 		//load modle
 		var fileJson={};
-		var _fun_createImage=function(tempJson){
+
+		function _createImage(tempJson){
 			var img=document.createElement("img");
 				img.src=tempJson.url;
 				img.onload=function(){
@@ -158,17 +206,17 @@ var game=window.game||(function(){
 				};
 		};
 		//child object
-		var child={};
+		var spriteList={};
+		Animation.setChildList(spriteList);
 
 
 		//load modle
 		var _fun_createSound=function(){
 		};
 		return {
-			funLib:_funlib,
+			funLib:_funLib,
 			start:function(){
-				Animation.setChildList=child;
-				Animation.start(canvas);		
+				Animation.start();		
 			},
 			togglePaused:function(){
 						 
@@ -179,16 +227,18 @@ var game=window.game||(function(){
 			removeSprite:function(name){
 				delete spriteList[name];
 			},
-			addSprite:function(name,sprite){
-				spriteList[name]=sprite;
+			addSprite:function(sprite){
+				spriteList[sprite.name]=sprite;
 			},
 			getSpriteEntity:function(){
 				return Sprite;
-						   },
+			},
 			addFile:function(Ary){
-				for(var i=0,l=Ary.length;i++){
-					var tmepObj=Ary[i];
-					fileJson[tempObj.type][tempObj.name]=tempObj;
+				for(var i=0,l=Ary.length;i<l;i++){
+					var tempObj=Ary[i];
+					var tempName={};
+						tempName[tempObj.name]=tempObj;
+					fileJson[tempObj.type]=tempName;
 				};
 				return this;
 			},
@@ -196,13 +246,20 @@ var game=window.game||(function(){
 				var imgJson=fileJson.img,
 					soundJson=fileJson.sound;
 					for(var i in imgJson){
-						_fun_createImag(imgJson[i]);	
+						_createImage(imgJson[i]);	
 					}
 					for(var i in soundJson){
 						_fun_createSound(soundJson[i]);
 					}
 				 },
-			getElement:function(){
+			getImage:function(name){
+				return fileJson["img"][name].obj;
+			},
+			getSound:function(){
+			},
+			appendTo:function(str){
+				var box=document.getElementById(str);
+					box.appendChild(Animation.getCanvas());
 					   
 					   }
 		}
@@ -210,21 +267,19 @@ var game=window.game||(function(){
 	return _game;
 })();
 
-
 game.addFile([
 	{
 		"type":"img"
 		,"name":"monkey"
-		,"url":"/img/abc.png"
-		,"url":"abc"
+		,"url":"/image/people.jpg"
 	},
 	{
 		"type":"sound"
 		,"url":"/sound/sound.mp3"
-		,"url":"abc"
 	}
 ]).load();
 
+/*
 var sp=new Sprite();
 	sp.addInit("jump",{
 		"data":[
@@ -242,3 +297,26 @@ var sp=new Sprite();
 			]
 		,"img":game.getImage("songsong")
 	});
+	*/
+function init(){
+	game.appendTo("box");
+	var sprite=game.getSpriteEntity();
+	console.log(game.getImage("monkey"));
+	var s=new sprite(100,100);	
+		s.setImageData("jump",{
+			"data":[
+			{x:0,y:0,width:100,height:20}
+					],
+			"img":game.getImage("monkey")
+		});
+	var i=0.5;
+		s.setFrame("test",function(data){
+			this.spriteCtx.clearRect(0,0,this.width,this.height);
+			//this.spriteCtx.fillRect(i,i++,10,10);
+			this.spriteCtx.drawImage(this.ImageData["jump"][0],0,0);
+		},1000);
+	$("body").click(function(){
+	game.start();
+	
+	});	
+}
